d550bdbe62941a4fa04a75578c71fbc8fdcf7e5f
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-01-06 01:45:46 -0300

    sort of hardcoded but managed to consume the json file and render everything almost OK

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import items from './db.json';
 
 const Select = () => 
-  <select>{
+  <select className='input__form'>{
     items.items[3].options.map( (i) => 
       <option key={i.value}>{i.label}</option> )
   }</select>;
@@ -11,10 +11,48 @@ function App() {
   return (
     <div className='App' >
         <h1>Challenge</h1>
-        <h2>Interfaz de encuesta</h2>
-        <form className='form'>
-        {items.items.map( i => 
-          <>
+        <h2>Interfaz de encuesta</h2> 
+            <form className='form'>
+              <input
+                className='input__form name'
+                type={items.items[0].type}
+                placeholder={items.items[0].label}
+                name={items.items[0].name}
+                required
+              />
+              <input
+                 className='input__form mail'
+                 type={items.items[1].type}
+                 placeholder={items.items[1].label}
+                 name={items.items[1].name}
+                 required
+              />
+              <input
+                  className='input__form date'
+                  type={items.items[2].type}
+                  placeholder={items.items[2].label}
+                  name={items.items[2].name}
+                  required
+              /> 
+              <Select/>
+              <label className='label__checkbox'>
+                {items.items[4].label}
+                <input 
+                  type={items.items[4].type}
+                  name={items.items[4].name}
+                  required
+                  />
+              </label>
+              <button 
+                className='button__form'
+                type={items.items[5].type}
+                placeholder={items.items[5].label}
+              />
+                
+        {/*Nice to acomplish pero probare otra forma y luego vere si se puede refactorizar*/}
+        {/* {items.items.map( i => 
+          <> 
+          {/*label no se está renderizando:
           <label
             className='label__form'
             label={i.label}
@@ -27,12 +65,9 @@ function App() {
             key={i.name}
             placeholder={i.label}
           >
-            
           </input>
-          
           </>
-        )}
-        {/* <Select/> */}
+        )} */}
         </form>
     </div>
   );
