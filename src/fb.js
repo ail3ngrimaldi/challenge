ad08568e0dc62e130c3c2b888ab7e1c925283454
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-01-09 13:23:49 -0300

    trying another way to hide keys

--- src/fb.js
+++ src/fb.js
@@ -1,15 +1,9 @@
 import {  initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-//import firebase from '@firebase/app-compat';
+import { credentials } from './credentials.js'
 
 const firebaseConfig = initializeApp({ 
-  name: "dev project",
-  apiKey: "AIzaSyAA34doZVB365D0bpyz4qSrnaQM4SviLNg",
-  authDomain: "challenge-form-f0513.firebaseapp.com",
-  projectId: "challenge-form-f0513",
-  storageBucket: "challenge-form-f0513.appspot.com",
-  messagingSenderId: "627927299682",
-  appId: "1:627927299682:web:4b78c5bfbaac7be5b1033b"
+  credentials
  });
 
  let firebaseApp; 
