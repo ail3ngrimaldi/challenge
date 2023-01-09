86b60f67eb066765cf9cc971d2f9cad0db5dd237
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-01-09 13:40:09 -0300

    back to hardcoded keys

--- src/fb.js
+++ src/fb.js
@@ -1,9 +1,14 @@
 import {  initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { credentials } from './credentials.js'
 
 const firebaseConfig = initializeApp({ 
-  credentials
+  name: "dev project",
+  apiKey: "AIzaSyAA34doZVB365D0bpyz4qSrnaQM4SviLNg",
+  authDomain: "challenge-form-f0513.firebaseapp.com",
+  projectId: "challenge-form-f0513",
+  storageBucket: "challenge-form-f0513.appspot.com",
+  messagingSenderId: "627927299682",
+  appId: "1:627927299682:web:4b78c5bfbaac7be5b1033b"
  });
 
  let firebaseApp; 
