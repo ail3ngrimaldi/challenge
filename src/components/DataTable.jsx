ffb3d663d53f1ad19be4d7b0b245bc12816b9467
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-01-11 21:04:48 -0300

    added last field on table

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -5,21 +5,21 @@ const DataTable = ({data}) => {
         <table className='table'>
         <thead>
             <tr className='main__tablerow'>
-                <th>ID</th>
                 <th>Nombre completo</th>
                 <th>Email</th>
                 <th>País de origen</th>
                 <th>Fecha de nacimiento</th>
+                <th>Aceptó los términos y condiciones</th>
             </tr>
         </thead>
         <tbody>
             {data.map(item => (
                 <tr key={item.id}>
-                    <td>{item.id}</td>
                     <td>{item.full_name}</td>
                     <td>{item.email}</td>
                     <td>{item.country_of_origin}</td>
                     <td>{item.birth_date}</td>
+                    <td>{item.terms_and_conditions}</td>
                 </tr>
             ))}
         </tbody>
